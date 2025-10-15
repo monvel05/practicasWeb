44e9565c4d5c04fe2951d04dfe2fae2df83ec4fd
Mon <monica.velasco.6121@gmail.com>
2025-10-15 08:28:54 -0600

    Añadir funcionalidad para obtener personajes de Rick y Morty y Pikachu, y mejorar la estructura del HTML en la página principal.

--- practicaAng1/src/app/pagina-principal/pagina-principal.ts
+++ practicaAng1/src/app/pagina-principal/pagina-principal.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { Login } from "../login/login";
-import { MortyApi } from '../morty-api';
+import { MortyApi } from '../services/morty-api';
+import { PokeApi } from '../services/poke-api';
 
 @Component({
   selector: 'app-pagina-principal',
-  imports: [Login],
   templateUrl: './pagina-principal.html',
   styleUrl: './pagina-principal.css'
 })
 export class PaginaPrincipal {
-  constructor(private mortyApi: MortyApi) {}
+  constructor(private mortyApi: MortyApi, private pokeApi: PokeApi) {}
   nombre = 'Monica';
   animal = 'xd';
+  personajes: any[] = [];
+  pikachu: any = null;
+
   cambiarAnimal(){
     if(this.animal == 'gatito'){
       this.animal = 'perrito';
@@ -22,8 +25,14 @@ export class PaginaPrincipal {
   abarrotesPablito (){
     this.mortyApi.obtenerPersonajes().subscribe((response: any) => {
       console.log(response);
+      this.personajes = response.results;
     })
   }
 
-  
+  traerAPikachu(){
+    this.pokeApi.obtenerPikachu().subscribe((response: any) => {
+      console.log(response);
+      this.pikachu = response;
+    })
+  }
 }
